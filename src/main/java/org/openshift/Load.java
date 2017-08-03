50f02983c42551f5ce3a61d36a6c865faef20e7a
Michael Nagels <michaelnagels@hotmail.com>
2017-08-03 16:36:16 +0200

    test burner

--- src/main/java/org/openshift/Load.java
+++ src/main/java/org/openshift/Load.java
@@ -7,6 +7,7 @@ package org.openshift;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -29,6 +30,24 @@ public class Load extends HttpServlet{
         final long duration = 100000;
         for (int thread = 0; thread < numCore * numThreadsPerCore; thread++) {
             new BusyThread("Thread" + thread, load, duration).start();
+        }
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FileUpload</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet FileUpload at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
     }
     
